8849644ffaf8ff06d71aa18b498a68117591a0e1
Victor Santos <jvic71174@gmail.com>
2024-03-11 11:22:07 -0300

    Android

--- apps/EMPTY/app/src/main/java/com/example/empty/MainActivity.kt
+++ apps/EMPTY/app/src/main/java/com/example/empty/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.empty
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
 
-class MainActivity : ComponentActivity {
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    override fun onCreate(savedInstanceState: Bundle) {
 
     }
 
